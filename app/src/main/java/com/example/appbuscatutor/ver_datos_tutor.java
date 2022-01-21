26990606aa6e4767303d2f2a33c22c4011fdbfe9
johan_mitma12 <avira365@hotmail.com>
2022-01-21 18:17:28 -0500

    subir archivos

---
+++ app/src/main/java/com/example/appbuscatutor/ver_datos_tutor.java
@@ -0,0 +1,14 @@
+package com.example.appbuscatutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ver_datos_tutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_datos_tutor);
+    }
+}
