f32db7d32be5690bff7b838b48b759ce65bd941b
johan_mitma12 <avira365@hotmail.com>
2022-01-24 02:45:53 -0500

    gradle merge

--- app/src/main/java/com/example/appbuscatutor/MainActivity.java
+++ app/src/main/java/com/example/appbuscatutor/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button= (Button)findViewById(R.id.btnin);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                System.out.println("CLICK REALIZADO");
+                Intent intent= new Intent(getBaseContext(),homepage_tutorfavorito.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     /*Cambiar de activities sin usar botones */
     public void registrar_usuario(View view) {
         startActivity(new Intent(getApplicationContext(),registrar_usuario.class));
         finish();
+    }
+    public void homepage(View view){
+        Intent intent= new Intent(MainActivity.this,homepage.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/appbuscatutor/homepage.java
+++ app/src/main/java/com/example/appbuscatutor/homepage.java
@@ -11,7 +11,17 @@ import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -24,9 +34,12 @@ import java.util.List;
 
 public class homepage extends AppCompatActivity {
     private int id_usuario = 2;
+    private RequestQueue requestQueue;
+    private String URL_TUTORES_FAVORITOS="https://825tzl1d6f.execute-api.us-east-1.amazonaws.com/v1/tutores-favoritos?id=%d";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("ON CREATE");
 
         super.onCreate(savedInstanceState);
         //Para eliminar el BAR superior
@@ -35,13 +48,94 @@ public class homepage extends AppCompatActivity {
         //obtener el Intent de la actividad actual recien creada
         Intent intent = getIntent();
         //Obtener el ID usuario  que la interfaz anterior envio
-        id_usuario=Integer.parseInt(intent.getStringExtra("id_usuario"));
-        ///createItemTutores(10);
+        try{
+            id_usuario=Integer.parseInt(intent.getStringExtra("id_usuario"));
+        }catch(Exception e){
+            id_usuario=1;
+        }
+        System.out.println("Hola estoy aquia");
+        requestQueue= Volley.newRequestQueue(this);
 
-        new TaskTutoresFavoritos(id_usuario).execute();
+        List<TutoresFavoritos> tutores= tutoresFavoritosRequest(id_usuario);
+        for(TutoresFavoritos t: tutores){
+            System.out.println(t.getNombre());
+        }
+
+        buildItems(tutores);
+
+        //new TaskTutoresFavoritos(id_usuario).execute();
         System.out.println("TASK FINISHED");
 
+
     }
+    private void initUI(){
+
+    }
+    private List<TutoresFavoritos> tutoresFavoritosRequest(int id){
+        List<TutoresFavoritos> tutores=new ArrayList<>();
+        JsonObjectRequest responseTutores=new JsonObjectRequest(
+                Request.Method.GET,
+                String.format(URL_TUTORES_FAVORITOS, id),
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            int id_estudiante= response.getInt("id_estudiante");
+                            //el arreglo de tutors encontrados
+                            JSONArray jsonArray= response.getJSONArray("tutores");
+
+                            for(int i=0;i<jsonArray.length();i++){
+                                JSONObject jsonObject=new JSONObject(jsonArray.get(i).toString());
+                                String nombre=jsonObject.getString("nombre_completo");
+                                String descripcion=jsonObject.getString("descripcion");
+                                String foto=jsonObject.getString("foto");
+                                int id_tutor=jsonObject.getInt("id_tutor");
+                                tutores.add(new TutoresFavoritos(nombre,descripcion,foto,id_tutor));
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("Error de consulta de la API REST");
+                        System.out.println(error);
+                    }
+                }
+
+        );
+        requestQueue.add(responseTutores);
+
+        return tutores;
+    }
+
+    public void buildItems(List<TutoresFavoritos> tutores){
+        ListAdapter listAdapter= new ListAdapter(tutores, homepage.this, new ListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(TutoresFavoritos item) {
+                moveToPerfilTutor(item);
+            }
+        });
+        RecyclerView recyclerView= findViewById(R.id.lista_items);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(homepage.this));
+        recyclerView.setAdapter(listAdapter);
+    }
+    public void moveToPerfilTutor(TutoresFavoritos item){
+        Intent intent= new Intent(homepage.this,ver_datos_tutor.class);
+        intent.putExtra("id_tutor",String.valueOf(item.getId()));
+        startActivity(intent);
+    }
+
+
+
+
+
+
 /*
     private void createItemTutores(int n) {
         List<TutoresFavoritos> lista_tutores = new ArrayList<>();
@@ -57,7 +151,7 @@ public class homepage extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
     }
-*/
+
 
     class TaskTutoresFavoritos extends AsyncTask<Void, Void, Void> {
         private String stringConnection = "jdbc:mysql://buscatutordatabase.cuxsffuy95k9.us-east-1.rds.amazonaws.com:3306/buscatutor?UseUnicode=true&characterEncoding=utf8";
@@ -177,4 +271,8 @@ public class homepage extends AppCompatActivity {
             this.id = id;
         }
     }
+
+ */
+
+
 }
